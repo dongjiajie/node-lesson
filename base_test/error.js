093712d6be424a95d6e1fec9fae01277bf3884c7
Jacky <jazhang@xogrp.com>
2017-02-28 16:35:46 +0800

    add error test

--- base_test/error.js
+++ base_test/error.js
@@ -1,9 +1,15 @@
+const Boom = require('boom');
 const _ = require('lodash');
 
 function err() {
-	return new Error('it is a error.');
+	const e = new Error('it is a error.');
+	return e;
 }
 
 if (_.isError(err())) {
 	console.log(err().message);
 }
+
+const error = Boom.create(400, 'Bad Request');
+
+console.log(error.output.statusCode);
