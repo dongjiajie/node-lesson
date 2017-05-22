969b68ad2560f2d7a7078a59b9cb575000cabb72
Jacky <jazhang@xogrp.com>
2017-05-22 09:52:58 +0800

    fix data type test

--- base_test/fc_scope_test.js
+++ base_test/fc_scope_test.js
@@ -20,6 +20,42 @@ function pet() {
 	console.log(name);
 	console.log(dogName);
 	console.log(catName);
+	console.log(name);
 }
 
-pet()
+// pet()
+
+function showArray(ar) {
+	ar.push(4);
+	const newArray = ar.concat([5]);
+	console.log(ar)
+	return newArray;
+}
+
+function run() {
+	const ar = [1, 2, 3];
+	const result = showArray(ar);
+	console.log(ar)
+	console.log(result);
+}
+
+// run()
+
+function face(options) {
+	const newOptions = Object.assign({}, options);
+	newOptions.age = 90;
+	return newOptions;
+}
+
+function obj() {
+	const options = {
+		name: 'dog',
+		sex: 'male'
+	}
+
+	face(options);
+	console.log(options);
+}
+
+// obj();
+
