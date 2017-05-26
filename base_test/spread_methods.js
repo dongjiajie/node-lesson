383994df3320745844e6974a3a2b714fca005117
Jacky <jazhang@xogrp.com>
2017-05-26 12:53:24 +0800

    test three dot

--- base_test/spread_methods.js
+++ base_test/spread_methods.js
@@ -1,3 +1,24 @@
 const a = {a:'a'}
-const b = {...a}
-console.log(b)
+// const b = {...a}
+console.log(a)
+
+// array
+const ar = [1, 2, 3, 4];
+const val = [...ar];
+console.log(val)
+
+function dog(...name) {
+	return name;
+}
+
+// three dot
+const res = dog('a', 'b');
+console.log(res);
+
+console.log('=====');
+const r = [].concat(...[1, 2, [3]]);
+console.log(r);
+
+console.log('===>');
+const r1 = [].concat([1], [2], ...[3, ...[4, [5]]]);
+console.log(r1);
