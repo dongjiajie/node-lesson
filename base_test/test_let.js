87f14925f4ada42ce60bfc57a8f96b0f1f30518e
CNBlackJ <yes.heng@icloud.com>
2017-05-29 19:15:00 +0800

    scope of let & const

--- base_test/test_let.js
+++ base_test/test_let.js
@@ -14,4 +14,45 @@ function people() {
 	console.log(pet);
 }
 
-people()
+// people()
+
+function showMoney() {
+	const money = [1, 2, 3, 4, 500];
+
+	try {
+		for(let i = 0; i < money.length; i++) {
+			console.log(money[i]);
+		};
+		const log = `cannot read i: ${i}`;
+	} catch(error) {
+		console.log(error.message);
+	}
+
+	try {
+		for(const l = 0; l < money.length; l++) {
+			console.log(money[l]);
+		}
+	} catch(error) {
+		console.log(error.message);
+	}
+
+	try {
+		const m = 3;
+		for(let i = 0; i < m; i++) {
+			console.log(money[i]);
+		}
+	} catch(error) {
+		console.log(error.message);
+	}
+
+	try {
+		for(var l = 0; l < money.length; l++) {
+			console.log(money[l]);
+		}
+	} catch(error) {
+		console.log(`My value is l = ${l}`);
+	}
+
+}
+
+showMoney();
