678fc679e6d6d8e00769e09eb8d18e53e7ce47f0
CNBlackJ <yes.heng@icloud.com>
2017-07-01 00:24:00 +0800

    add joi array test

--- joi_test/array_unique.js
+++ joi_test/array_unique.js
@@ -6,6 +6,12 @@ const schema = Joi.array().unique((a,b) => {
 
 const schema2 = Joi.array().unique('a').unique('b')
 
-const res = Joi.validate([{a:1,b:7},{a:2, b:1},3], schema2);
+const schema3 = {
+	a: Joi.array().allow('b')
+}
+
+// const res = Joi.validate([{a:1,b:7},{a:2, b:1},3], schema2);
+const res = Joi.validate({ a: 'a' }, schema3)
 
 console.log(res);
+
