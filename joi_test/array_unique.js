493b5e24329401b0dd85c065fc590bb7be0d2c11
CNBlackJ <yes.heng@icloud.com>
2017-07-02 16:15:09 +0800

    joi valida

--- joi_test/array_unique.js
+++ joi_test/array_unique.js
@@ -7,7 +7,7 @@ const schema = Joi.array().unique((a,b) => {
 const schema2 = Joi.array().unique('a').unique('b')
 
 const schema3 = {
-	a: Joi.array().allow('b')
+	a: Joi.array().valid('b')
 }
 
 // const res = Joi.validate([{a:1,b:7},{a:2, b:1},3], schema2);
